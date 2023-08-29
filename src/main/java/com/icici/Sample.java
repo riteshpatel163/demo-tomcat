51059989141edb0e71ae0f6c64de02710b995d3a
lala patel <erlalapatel1996@gmail.com>
2023-08-29 21:11:21 +0530

    sjksfk

---
+++ src/main/java/com/icici/Sample.java
@@ -0,0 +1,14 @@
+package com.icici;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sample
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Patel!" );
+    }
+}
+
